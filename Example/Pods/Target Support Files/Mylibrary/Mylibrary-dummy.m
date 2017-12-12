d02d9d3ece31cebfe9c2734d466e7097d28574c3
yongjie_zou <zouyongjie@haixue.com>
2017-12-12 12:37:58 +0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/Mylibrary/Mylibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Mylibrary : NSObject
+@end
+@implementation PodsDummy_Mylibrary
+@end
---
+++ Example/Pods/Target Support Files/Pods-Mylibrary_Example/Pods-Mylibrary_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mylibrary_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Mylibrary_Example
+@end
